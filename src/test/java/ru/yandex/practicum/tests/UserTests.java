7bd11a461136e9ebf23fbde1a1117af7e58313e7
Alexandr Semenov <snakeage@yandex.ru>
2025-10-10 19:56:24 +0300

    Добавил UserTests.java

--- src/test/java/ru/yandex/practicum/tests/UserTests.java
+++ src/test/java/ru/yandex/practicum/tests/UserTests.java
@@ -1,4 +1,81 @@
 package ru.yandex.practicum.tests;
 
-public class UserTests {
+import io.qameta.allure.junit4.DisplayName;
+import io.restassured.response.Response;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.Test;
+import ru.yandex.practicum.model.User;
+import ru.yandex.practicum.steps.ApiSteps;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class UserTests extends BaseTest {
+
+    private final ApiSteps apiSteps = new ApiSteps();
+    private String accessToken;
+
+    @Test
+    @DisplayName("Создание уникального пользователя")
+    public void createUniqueUserTest() {
+        String email = RandomStringUtils.randomAlphabetic(10) + "@yandex.ru";
+        User user = new User(email, "password123", "TestUser");
+
+        Response response = given()
+                .header("Content-Type", "application/json")
+                .body(user)
+                .post("/api/auth/register");
+
+        response.then()
+                .statusCode(200)
+                .body("success", equalTo(true))
+                .body("user.email", equalTo(email))
+                .body("user.name", equalTo("TestUser"));
+
+        // Очистка
+        accessToken = response.then().extract().path("accessToken");
+        apiSteps.deleteUser(accessToken);
+    }
+
+    @Test
+    @DisplayName("Создание пользователя, который уже зарегистрирован")
+    public void createExistingUserTest() {
+        String email = RandomStringUtils.randomAlphabetic(10) + "@yandex.ru";
+        String password = "password123";
+        String name = "TestUser";
+
+        // Создаем
+        accessToken = apiSteps.registerAndGetToken(email, password, name);
+
+        // Повторно
+        User user = new User(email, password, name);
+        Response response = given()
+                .header("Content-Type", "application/json")
+                .body(user)
+                .post("/api/auth/register");
+
+        response.then()
+                .statusCode(403)
+                .body("success", equalTo(false))
+                .body("message", equalTo("User already exists"));
+
+        // Очистка
+        apiSteps.deleteUser(accessToken);
+    }
+
+    @Test
+    @DisplayName("Создание пользователя без обязательного поля")
+    public void createUserWithoutRequiredFieldTest() {
+        User user = new User("", "password123", "TestUser");
+
+        Response response = given()
+                .header("Content-Type", "application/json")
+                .body(user)
+                .post("/api/auth/register");
+
+        response.then()
+                .statusCode(403)
+                .body("success", equalTo(false))
+                .body("message", equalTo("Email, password and name are required fields"));
+    }
 }
