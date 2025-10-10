0d11af06a904b486e5212e1744e1d4aa64f973ba
Alexandr Semenov <snakeage@yandex.ru>
2025-10-10 19:44:44 +0300

    Добавил BaseTest.java

--- src/test/java/ru/yandex/practicum/tests/BaseTest.java
+++ src/test/java/ru/yandex/practicum/tests/BaseTest.java
@@ -1,4 +1,11 @@
 package ru.yandex.practicum.tests;
 
+import org.junit.Before;
+import ru.yandex.practicum.config.RestConfig;
+
 public class BaseTest {
+    @Before
+    public void setUp() {
+        RestConfig.setUp();
+    }
 }
