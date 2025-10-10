9f416ee2b07b96e426e1884c2c826208c3f9fba1
Alexandr Semenov <snakeage@yandex.ru>
2025-10-10 19:42:34 +0300

    Добавил Ingredient.java

--- src/main/java/ru/yandex/practicum/model/Ingredient.java
+++ src/main/java/ru/yandex/practicum/model/Ingredient.java
@@ -1,4 +1,19 @@
 package ru.yandex.practicum.model;
 
+import lombok.Data;
+
+@Data
 public class Ingredient {
+    private String _id;
+    private String name;
+    private String type;
+    private int proteins;
+    private int fat;
+    private int carbohydrates;
+    private int calories;
+    private float price;
+    private String image;
+    private String image_mobile;
+    private String image_large;
+    private String __v;
 }
