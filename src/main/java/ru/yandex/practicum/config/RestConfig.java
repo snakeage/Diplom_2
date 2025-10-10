f1c57edeff5a28906ade8fcc00861429f7300de7
Alexandr Semenov <snakeage@yandex.ru>
2025-10-10 19:55:27 +0300

    Добавил RestConfig.java

--- src/main/java/ru/yandex/practicum/config/RestConfig.java
+++ src/main/java/ru/yandex/practicum/config/RestConfig.java
@@ -1,4 +1,12 @@
 package ru.yandex.practicum.config;
 
+import io.restassured.RestAssured;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+
 public class RestConfig {
+    public static void setUp() {
+        RestAssured.baseURI = "https://stellarburgers.education-services.ru";
+        RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
+    }
 }
