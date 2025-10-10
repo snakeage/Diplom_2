4b37e17fd971e45a52239bbffe8acec7d8977285
Alexandr Semenov <snakeage@yandex.ru>
2025-10-10 19:41:39 +0300

    Добавил Order.java

--- src/main/java/ru/yandex/practicum/model/Order.java
+++ src/main/java/ru/yandex/practicum/model/Order.java
@@ -1,4 +1,14 @@
 package ru.yandex.practicum.model;
 
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class Order {
+    private List<String> ingredients;
+
+    public Order(List<String> ingredients) {
+        this.ingredients = ingredients;
+    }
 }
